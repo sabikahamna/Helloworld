dbb22f9dbf10696255695640f7007df73d0a5c37
sabikahamna <sabikahamna@gmail.com>
2024-11-29 15:43:20 +0500

    Add files via upload

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+      <div>
+  
+        <h1>HELLO guys</h1>  
+        <h1>this is my first website</h1> 
+                            
+      </div>  
+      
+    )
+  }
+  
---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+
+export default function Home() {
+  return (
+    <div>
+            
+      <h1>Hello world</h1>   
+      <h1> This is me Fatima khan, my first website is here!</h1>
+                           
+     </div>  
+    
+  )
+}
+
+
+
+
+
+
+
